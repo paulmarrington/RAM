457574a4381d9be5d7244590ef21f44be8820145
Paul Marrington <paul@marrington.net>
2016-02-29 11:31:29 +1100

    no need to authenticate mongo for dev

--- backend/conf/conf.js
+++ backend/conf/conf.js
@@ -2,6 +2,6 @@ module.exports = {
     "frontendDir": "../../../../frontend/dist",
     "logDir" : "log",
     "httpPort":3000,
-    "mongoURL" : "mongodb://username:password@127.0.0.1:27017/ramdb",
+    "mongoURL" : "mongodb://127.0.0.1:27017/ramdb",
     "devMode":true,
 };
