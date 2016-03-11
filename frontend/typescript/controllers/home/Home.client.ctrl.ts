fef81763e9b7c73b16876a4752c9dc29fade073a
Ali Salehi <salehiam@gmail.com>
2016-03-11 11:23:23 +1100

    Added pagination to sample ng-data table Using dummy data and not the new data structure yet Changes to ram data structures added ng-table to frontend.

--- frontend/typescript/controllers/home/Home.client.ctrl.ts
+++ frontend/typescript/controllers/home/Home.client.ctrl.ts
@@ -32,9 +32,9 @@ export class HomeCtrl {
         };
 
         $scope.helpers = cUtils.Helpers;
-        let relations = restNg.all("relations");
-        relations.one("123").getList().then((individual_business_authorisations: Array<cApi.IndividualBusinessAuthorisation>) => {
-            $scope.individual_business_authorisations = individual_business_authorisations;
-        });
+        // let relations = restNg.all("relations");
+        // relations.one("123").getList().then((individual_business_authorisations: Array<cApi.IndividualBusinessAuthorisation>) => {
+        //     $scope.individual_business_authorisations = individual_business_authorisations;
+        // });
     }
 }
