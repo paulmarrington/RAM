6a4c10e811aff9bbec5666e51d60c0edef2c2a53
Ali Salehi <salehiam@gmail.com>
2016-03-22 15:52:22 +1100

    Add typescript to sourcemap source so it can be debugged. Removed ng-table plugin, rewrote the table plugin in angular Added mobile support Added responsive css layout helpers Improved RamAPI by adding common respond type Added async loading to the table Added ram rest angualr service remove party relations page and its related files renamed partylist controler and resources to party

--- frontend/typescript/_ClientTypes.ts
+++ frontend/typescript/_ClientTypes.ts
@@ -2,10 +2,10 @@
 /// <reference path="../../commons/_CommonTypes.ts" />
 /// <reference path="api/IRamScope.ts" />
 /// <reference path="controllers/Layout.client.ctrl.ts" />
-/// <reference path="controllers/partyList/PartyList.client.ctrl.ts" />
-/// <reference path="controllers/partyRelations/PartyRelations.client.ctrl.ts" />
+/// <reference path="controllers/party/Party.client.ctrl.ts" />
 /// <reference path="controllers/home/Home.client.ctrl.ts" />
 /// <reference path="controllers/404/FourOFour.client.ctrl.ts" />
 /// <reference path="widgets/RelationshipTable.widget.ts" />
+/// <reference path="services/RAMRestServices.client.ts" />
 
 /// <reference path="App.ts" />
