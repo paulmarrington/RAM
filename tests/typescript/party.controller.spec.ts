80cc7900a814be5feed18cd429b47c6ccfea9e03
Paul Marrington <paul@marrington.net>
2016-05-04 14:31:22 +1000

    preparing for ts conversion of tests

--- tests/typescript/party.controller.spec.ts
+++ tests/typescript/party.controller.spec.ts
@@ -1,59 +1,65 @@
-var partyHelper = require("../../spec/support/party")
-rest = require("../../spec/support/rest.js")
+import {
+  new_party, fake_abn, new_two_identity_party,
+  get_party, update_party
+}  from '../spec/support/party';
 
-describe("a RAM Party", () => {
-  it("can create a new identity and party", function(done) {
-    partyHelper.new_party(partyHelper.fake_abn()).then(function (result) {
-      expect(result).toEqual(jasmine.objectContaining({ deleted: false }))
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can create a new identity for an existing party", function(done) {
-    partyHelper.new_two_identity_party(partyHelper.fake_abn()).then(function(result) {
-      expect(result.identities.length).toEqual(2)
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can retrieve a party based on identity", function(done) {
-    var abn = partyHelper.fake_abn()
-    partyHelper.new_party(abn).then(function (result) {
-      partyHelper.get_party(abn).then(function (result) {
-        expect(result.identities.length).toEqual(1)
-        done()
-      }).catch(function(err) { fail(err); done() })
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can retrieve a list of identities for a party", function(done) {
-    partyHelper.new_two_identity_party(partyHelper.fake_abn()).then(function(result) {
-      expect(result.identities.length).toEqual(2)
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can change party roles", function(done) {
-    partyHelper.update_party(partyHelper.fake_abn(), {
-        $addToSet: {roles: {name: "spouse"}},
-        $set:      {"attributes.magic": "dark"}
+const contains = (obj) => jasmine.objectContaining(obj);
+
+describe('a RAM Party', () => {
+  it('can create a new identity and party', (done) => {
+    new_party(fake_abn()).then((result) => {
+      expect(result).toEqual(contains({ deleted: false }));
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
+  it('can create a new identity for an existing party', (done) => {
+    new_two_identity_party(fake_abn()).then((result) => {
+      expect(result.identities.length).toEqual(2);
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
+  it('can retrieve a party based on identity', (done) => {
+    const abn = partyHelper.fake_abn();
+    partyHelper.new_party(abn).then((result) => {
+      get_party(abn).then((result) => {
+        expect(result.identities.length).toEqual(1);
+        done();
+      }).catch((err) => { fail(err); done(); });
+    }).catch((err) => { fail(err); done(); });
+  });
+  it('can retrieve a list of identities for a party', (done) => {
+    new_two_identity_party(partyHelper.fake_abn()).then((result) => {
+      expect(result.identities.length).toEqual(2);
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
+});
+describe('more RAM Party', () => {
+  it('can change party roles', (done) => {
+    update_party(fake_abn(), {
+        $addToSet: {roles: {name: 'spouse'}},
+        $set:      {'attributes.magic': 'dark'}
       }
-    ).then(function(partyDoc) {
-      expect(partyDoc.roles.length).toEqual(2)
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can change party attributes", function(done) {
-    partyHelper.update_party(partyHelper.fake_abn(), {
-      "attributes.magic": "light"
-    }).then(function(partyDoc) {
-      expect(partyDoc.attributes.magic).toEqual("light")
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
-  it("can delete identities", function(done) {
-    var abn_1 = partyHelper.fake_abn()
-    partyHelper.update_party(abn_1, {
-      $pull: {identities: {value:abn_1, type:"abn"}},
-    }).then(function (updatedParty) {
-      expect(updatedParty.identities.length).toEqual(1)
-      done()
-    }).catch(function(err) { fail(err); done() })
-  })
+    ).then( (partyDoc) => {
+      expect(partyDoc.roles.length).toEqual(2);
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
+  it('can change party attributes', (done) => {
+    update_party(fake_abn(), {
+      'attributes.magic': 'light'
+    }).then((partyDoc) => {
+      expect(partyDoc.attributes.magic).toEqual('light');
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
+  it('can delete identities', (done) => {
+    const abn_1 = fake_abn();
+    update_party(abn_1, {
+      $pull: {identities: {value:abn_1, type:'abn'}},
+    }).then((updatedParty) => {
+      expect(updatedParty.identities.length).toEqual(1);
+      done();
+    }).catch((err) => { fail(err); done(); });
+  });
 });
--- tests/typescript/support/rest.ts
+++ tests/typescript/support/rest.ts
@@ -1,62 +1,47 @@
-var http = require("http");
+import * as http  from 'http';
 
-var serialize = function(obj ){
-  return '?'+Object.keys(obj).reduce(function(a,k){a.push(k+'='+encodeURIComponent(obj[k]));return a},[]).join('&')
-}
-
-var options = function(method, api) {
+const options = (method: string, api: string) => {
   return {
     method:   method,
-    host:     "localhost",
+    host:     'localhost',
     port:     3000,
-    path:     "/api/1/"+api  }
-}
+    path:     '/api/1/'+api ,
+    headers:  {}
+  };
+};
 
-var request = function(method, api, body) {
-  var opts = options(method, api)
+const request = (method: string, api: string, body?: {}) => {
+  const opts = options(method, api);
+  const body_as_json = body ? JSON.stringify(body) : '';
   if (body) {
-    body = JSON.stringify(body)
     opts.headers = {
       'Content-Type':   'application/json',
-      'Content-Length': body.length
-    }
+      'Content-Length': body_as_json.length
+    };
   }
-  return new Promise(function(resolve, reject) {
-    var data = []
-    var getter = http.request(opts, function(response) {
+  return new Promise((resolve, reject) => {
+    const data = [];
+    const getter = http.request(opts, (response) => {
       if (response.statusCode !== 200) {
-        return reject(response.statusMessage)
+        return reject(response.statusMessage);
       }
-      response.on("data", function(chunk) {
-        data.push(chunk)
-      })
-      response.on("end", function() {
-        var response = JSON.parse(data.join(""))
-        resolve(response.data ? response.data : response)
-      })
-    })
-    getter.end(body)
-    getter.on("error", function(err) { reject(err) })
-  })
-}
+      response.on('data', (chunk) => data.push(chunk));
+      response.on('end', () => {
+        const response = JSON.parse(data.join(''));
+        resolve(response.data ? response.data : response);
+      });
+    });
+    getter.end(body_as_json);
+    getter.on('error', (err) => reject(err));
+  });
+};
 
-module.exports = {
-  get: function(api) {
-    return request("GET", api)
-  },
-  post: function(api, body) {
-    return request("POST", api, body)
-  },
-  put: function(api, body) {
-    return request("PUT", api, body)
-  },
-  delete: function(api) {
-    return request("DELETE", api)
-  },
-  uuid: function() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+export const get = (api: string) => request('GET', api);
+export const post = (api: string, body: {}) => request('POST', api, body);
+export const put = (api: string, body: {}) => request('PUT', api, body);
+export const deleter = (api: string) => request('DELETE', api);
+export const uuid = () =>
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
       return v.toString(16);
     });
-  }
-}
