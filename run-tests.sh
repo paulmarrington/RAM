ee2078133f9dfa83902f64610035e62fe1d29668
Trung Hoang <trung@mobiusly.com>
2016-05-24 14:47:21 +1000

    Use typings inside .bin for build

--- run-tests.sh
+++ run-tests.sh
@@ -3,13 +3,13 @@ set -ev
 
 cd frontend
 npm install
-typings install
+node_modules/.bin/typings install
 jspm cc
 jspm install -y
 
 cd ../backend
 npm install
-typings install
+node_modules/.bin/typings install
 gulp serve &
 
 cd ../tests
