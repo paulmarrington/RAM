0d4f7afbb1dc674bd35d62ea586a229d17ac2ef8
Dave Cheong <dave@mobiusly.com>
2016-05-25 15:00:52 +1000

    Disabling (xit) a do nothing test

--- backend/typescript/tests/relationship.model.spec.ts
+++ backend/typescript/tests/relationship.model.spec.ts
@@ -7,7 +7,7 @@ describe('RAM Relationship', () => {
     connectDisconnectMongo();
 
     // todo replace this with something meaningful
-    it('spikes mongo connectivity', async (done) => {
+    xit('spikes mongo connectivity', async (done) => {
         try {
             const objectId = new mongoose.Types.ObjectId('5743cfe831286f0250b87da6');
             await RelationshipModel.getRelationshipById(objectId);
