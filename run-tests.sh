3919300c85e32c51367882ecb2acc076ec621f0b
Trung Hoang <trung@mobiusly.com>
2016-05-26 15:13:04 +1000

    Travis CI build fix - Justification comment of setting auth token in script

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -ev
 
-if [ $TRAVIS_PULL_REQUEST != "false" ]; then
-    echo "Pull request detected"
+if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]; then
 
+    # Force use of jspm registry auth token here due to the following situation:
+    # - secure vars not available on pull requests which are classified by travis as untrusted builds
+    # - jspm requires github authentication to avoid github rate limiting
+    # - normally the github auth token is encrypted and put in .travis.yml however it's not available here
+    # - all non upstream repo forks should provide their own JSPM_GITHUB_AUTH_TOKEN in travis environment variables
     jspm config registries.github.auth dHJ1bmdpZTp1eWVuMzQ=
 
 fi
