4ba6c3f35351a69045a729a3bd86025a92acf10e
Trung Hoang <trung@mobiusly.com>
2016-05-26 19:44:50 +1000

    Use locally installed jspm for configuring the auth token after npm install

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ev
 
+dir=$(pwd)
+export RAM_CONF=$dir/backend/conf/conf.js
+echo $RAM_CONF
+
+cd frontend
+npm install
 if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]; then
 
     # Force use of jspm registry auth token here due to the following situation:
@@ -9,17 +15,12 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]; then
     # - normally the github auth token is encrypted and put in .travis.yml however it's not available here
     # - all non upstream repo forks should provide their own JSPM_GITHUB_AUTH_TOKEN in travis environment variables
 
+    echo "Secure environment variables not available. Probably a pull request. Using default auth key."
+
     # auth key owned by @sundriver - public_repo only
     node_modules/.bin/jspm config registries.github.auth c3VuZHJpdmVyOjI4NTU3ZjlkNzdhZGU4YjJhODk3NDQyOTEyMzU5NDY0ZDBjMjkwYmE=
 
 fi
-
-dir=$(pwd)
-export RAM_CONF=$dir/backend/conf/conf.js
-echo $RAM_CONF
-
-cd frontend
-npm install
 node_modules/.bin/typings install
 node_modules/.bin/jspm -v
 node_modules/.bin/jspm cc
