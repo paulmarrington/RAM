b16e37fcee790d26a109e27c48e2296be6a1b3ae
Paul Marrington <paul@marrington.net>
2016-05-27 18:01:42 +1000

    activating add relationships page (started only)

--- frontend/typescript/components/add-relationship/add-relationship.component.ts
+++ frontend/typescript/components/add-relationship/add-relationship.component.ts
@@ -6,5 +6,33 @@ import {Component} from '@angular/core';
     directives: []
 })
 export class AddRelationshipComponent {
+  public isIndividual        = false;
+  public showForIndividual   = false;
+  public showForOrganisation = false;
+  public showSteps           = [true,true];
 
+  private clearSteps(from:number, to:number) {
+    for (let i = from; i <= to; i += 1) {
+      this.showSteps[i] = false;
+    }
+  }
+
+  private showStep(step:number) {
+    this.showSteps[step] = true;
+  }
+
+  private setRelationshipType(isIndividual:boolean) {
+    this.isIndividual = this.showForIndividual = isIndividual;
+    this.showForOrganisation = !isIndividual;
+    this.clearSteps(2, 5)
+  }
+
+  public setToIndividual() {
+    this.setRelationshipType(true);
+    this.showStep(2);
+  }
+
+  public setToOrganisation() {
+    this.setRelationshipType(false);
+  }
 }
