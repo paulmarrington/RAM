6fc84b832bb0e261b4830c347535b3b608b27cf1
Dave Cheong <dave@mobiusly.com>
2016-06-01 09:40:30 +1000

    Added missing endDate

--- backend/typescript/controllers/relationshipType.controller.ts
+++ backend/typescript/controllers/relationshipType.controller.ts
@@ -26,6 +26,7 @@ export class RelationshipTypeController {
                     shortDecodeText: attributeNameUsage.attributeName.shortDecodeText,
                     longDecodeText: attributeNameUsage.attributeName.longDecodeText,
                     startDate: attributeNameUsage.attributeName.startDate,
+                    endDate: attributeNameUsage.attributeName.endDate,
                     name: attributeNameUsage.attributeName.shortDecodeText,
                     domain: attributeNameUsage.attributeName.domain,
                     mandatory: !attributeNameUsage.optionalInd,
