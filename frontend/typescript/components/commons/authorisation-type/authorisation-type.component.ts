d7bd44691a820a7a1df546d2b9b6306926d70f3c
Paul Marrington <paul@marrington.net>
2016-06-06 11:30:17 +1000

    GUI - new relationship step 2 activation

--- frontend/typescript/components/commons/authorisation-type/authorisation-type.component.ts
+++ frontend/typescript/components/commons/authorisation-type/authorisation-type.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { ControlGroup, Control }
+from '@angular/common';
 
 @Component({
     selector: 'authorisation-type',
     templateUrl: 'authorisation-type.component.html'
 })
 export class AuthorisationTypeComponent {
+    private authTypeControl = new Control('Universal representative');
+
+    public form = new ControlGroup({
+        authType: this.authTypeControl
+    });
+    // Triggers if user changes selection for DDL
+    public onChange(authType:string) {
+        // TODO: once forms are connected...
+        alert(authType);
+    }
 }
