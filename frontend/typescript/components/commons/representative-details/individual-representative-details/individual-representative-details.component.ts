064ca5c12e8ae33d9a74ef90780e0316a8553a62
Paul Marrington <paul@marrington.net>
2016-06-07 17:18:23 +1000

    restore saved version

--- frontend/typescript/components/commons/representative-details/individual-representative-details/individual-representative-details.component.ts
+++ frontend/typescript/components/commons/representative-details/individual-representative-details/individual-representative-details.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { ControlGroup, Validators, Control }
+from '@angular/common';
 
 @Component({
     selector: 'individual-representative-details',
     templateUrl: 'individual-representative-details.component.html'
 })
 export class IndividualRepresentativeDetailsComponent {
+    private firstNameControl = new Control('', Validators.required);
+    private lastNameControl  = new Control('', Validators.required);
+    private dobControl       = new Control('');
+    public individualRepresentativeDetailsForm = new ControlGroup({
+        firstName: this.firstNameControl,
+        lastName:  this.lastNameControl,
+        dob:       this.dobControl
+    });
 }
--- frontend/typescript/components/commons/representative-details/representative-details.component.ts
+++ frontend/typescript/components/commons/representative-details/representative-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FORM_DIRECTIVES, FormBuilder } from '@angular/common';
+import { RAMComponent }                 from '../../../commons/RAMComponent';
 import {IndividualRepresentativeDetailsComponent} from
 './individual-representative-details/individual-representative-details.component';
 import {OrganisationRepresentativeDetailsComponent} from
@@ -9,8 +11,34 @@ import {OrganisationRepresentativeDetailsComponent} from
     templateUrl: 'representative-details.component.html',
     directives: [
         IndividualRepresentativeDetailsComponent,
-        OrganisationRepresentativeDetailsComponent
+        OrganisationRepresentativeDetailsComponent,
+        FORM_DIRECTIVES
     ]
 })
-export class RepresentativeDetailsComponent {
+
+export class RepresentativeDetailsComponent extends RAMComponent<RepresentativeDetailsComponentData> {
+
+    constructor(_fb: FormBuilder) { super(_fb); };
+
+    protected controls() {
+        return {
+            'authType': [this.data.representativeType]
+        };
+    }
+
+    // TODO: work-around until Angular-2 understands radio buttons
+    //private model:{selectRepresentativeType:String};
+    // constructor() {
+    //     this.model = {selectRepresentativeType: ''};
+    // }
+    get isIndividual(): boolean {
+        return this.data.representativeType === 'individual';
+    }
+    get isOrganisation(): boolean {
+        return this.data.representativeType === 'organisation';
+    }
+}
+
+export interface RepresentativeDetailsComponentData {
+    representativeType: String;
 }
