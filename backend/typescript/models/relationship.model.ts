00648779e4c30fc27fcd4c28fae25d47541cdf24
Dave Cheong <dave@mobiusly.com>
2016-06-08 15:04:21 +1000

    Added relationship model

---
+++ backend/typescript/models/relationship.model.ts
@@ -0,0 +1,113 @@
+import * as mongoose from 'mongoose';
+import {RAMEnum, IRAMObject, RAMSchema} from './base';
+import {IParty, PartyModel} from './party.model';
+import {IName, NameModel} from './name.model';
+
+// force schema to load first (see https://github.com/atogov/RAM/pull/220#discussion_r65115456)
+
+/* tslint:disable:no-unused-variable */
+const _PartyModel = PartyModel;
+
+/* tslint:disable:no-unused-variable */
+const _NameModel = NameModel;
+
+// enums, utilities, helpers ..........................................................................................
+
+export class RelationshipStatus extends RAMEnum {
+
+    public static Invalid = new RelationshipStatus('INVALID');
+    public static Pending = new RelationshipStatus('PENDING');
+    public static Active = new RelationshipStatus('ACTIVE');
+    public static Deleted = new RelationshipStatus('DELETED');
+    public static Cancelled = new RelationshipStatus('CANCELLED');
+
+    protected static AllValues = [
+        RelationshipStatus.Active,
+        RelationshipStatus.Cancelled,
+        RelationshipStatus.Deleted,
+        RelationshipStatus.Invalid,
+        RelationshipStatus.Pending
+    ];
+
+    constructor(name:String) {
+        super(name);
+    }
+}
+
+// schema .............................................................................................................
+
+const RelationshipSchema = RAMSchema({
+    subject: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Party',
+        required: [true, 'Subject is required']
+    },
+    subjectNickName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Name',
+        required: [true, 'Subject Nick Name is required']
+    },
+    delegate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Party',
+        required: [true, 'Subject is required']
+    },
+    delegateNickName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Name',
+        required: [true, 'Delegate Nick Name is required']
+    },
+    startTimestamp: {
+        type: Date,
+        required: [true, 'Start Timestamp is required']
+    },
+    endTimestamp: {
+        type: Date
+    },
+    endEventTimestamp: {
+        type: Date,
+        required: [function () {
+            return this.status === RelationshipStatus.Invalid ||
+                this.status === RelationshipStatus.Deleted ||
+                this.status === RelationshipStatus.Cancelled;
+        }, 'End Event Timestamp is required']
+    },
+    status: {
+        type: String,
+        required: [true, 'Status is required'],
+        trim: true,
+        enum: RelationshipStatus.valueStrings()
+    }
+});
+
+// interfaces .........................................................................................................
+
+export interface IRelationship extends IRAMObject {
+    subject: IParty;
+    subjectNickName: IName;
+    delegate: IParty;
+    delegateNickName: IName;
+    startTimestamp: Date;
+    endTimestamp: Date;
+    endEventTimestamp: Date;
+    status: string;
+    statusEnum(): RelationshipStatus;
+}
+
+/* tslint:disable:no-empty-interfaces */
+export interface IRelationshipModel extends mongoose.Model<IRelationship> {
+}
+
+// instance methods ...................................................................................................
+
+RelationshipSchema.method('statusEnum', function () {
+    return RelationshipStatus.valueOf(this.status);
+});
+
+// static methods .....................................................................................................
+
+// concrete model .....................................................................................................
+
+export const RelationshipModel = mongoose.model(
+    'Relationship',
+    RelationshipSchema) as IRelationshipModel;
--- backend/typescript/tests/relationship.model.spec.ts
+++ backend/typescript/tests/relationship.model.spec.ts
@@ -1,19 +1,85 @@
 import * as mongoose from 'mongoose';
-import {connectDisconnectMongo} from './helpers';
-import {RelationshipModel} from '../models/relationship-old.model';
+import {connectDisconnectMongo, dropMongo} from './helpers';
+import {
+    IName,
+    NameModel} from '../models/name.model';
+import {
+    IParty,
+    PartyModel,
+    PartyType} from '../models/party.model';
+import {
+    IRelationship,
+    RelationshipModel,
+    RelationshipStatus} from '../models/relationship.model';
 
+/* tslint:disable:max-func-body-length */
 describe('RAM Relationship', () => {
 
     connectDisconnectMongo();
+    dropMongo();
 
-    // todo replace this with something meaningful
-    xit('spikes mongo connectivity', async (done) => {
+    let subjectNickName1: IName;
+    let subjectParty1: IParty;
+
+    let delegateNickName1: IName;
+    let delegateParty1: IParty;
+
+    beforeEach(async (done) => {
+
         try {
-            const objectId = new mongoose.Types.ObjectId('5743cfe831286f0250b87da6');
-            await RelationshipModel.getRelationshipById(objectId);
+
+            subjectNickName1 = await NameModel.create({
+                givenName: 'Jane',
+                familyName: 'Subject 1'
+            });
+
+            subjectParty1 = await PartyModel.create({
+                partyType: PartyType.Individual.name,
+                name: subjectNickName1
+            });
+
+            delegateNickName1 = await NameModel.create({
+                givenName: 'John',
+                familyName: 'Delegate 1'
+            });
+
+            delegateParty1 = await PartyModel.create({
+                partyType: PartyType.Individual.name,
+                name: delegateNickName1
+            });
+
             done();
+
         } catch (e) {
-            fail();
+            fail('Because ' + e);
+            done();
+        }
+
+    });
+
+    it('inserts with valid values', async (done) => {
+        try {
+
+            const instance = await RelationshipModel.create({
+                subject: subjectParty1,
+                subjectNickName: subjectNickName1,
+                delegate: delegateParty1,
+                delegateNickName: delegateNickName1,
+                startTimestamp: new Date(),
+                status: RelationshipStatus.Active.name
+            });
+
+            expect(instance).not.toBeNull();
+            expect(instance.id).not.toBeNull();
+            expect(instance.subject).not.toBeNull();
+            expect(instance.status).not.toBeNull();
+            expect(instance.statusEnum()).toBe(RelationshipStatus.Active);
+
+            done();
+
+        } catch (e) {
+            fail('Because ' + e);
+            done();
         }
     });
 
