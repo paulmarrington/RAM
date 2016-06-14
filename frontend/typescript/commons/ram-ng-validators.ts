a713d9afbaba824c360e11d29fb5de04eced1595
Ali Salehi <ali.salehi@ato.gov.au>
2016-06-14 10:56:21 +1000

    added ng validation for udn and email added add-relationship-code page (UI) - First cut the path is available at /relationships/add-code

--- frontend/typescript/commons/ram-ng-validators.ts
+++ frontend/typescript/commons/ram-ng-validators.ts
@@ -2,33 +2,33 @@ import {Control} from '@angular/common';
 import {Utils} from '../../../commons/ram-utils';
 
 export class RAMNgValidators {
-    public static dateFormatValidator(c: Control) {
-        let v = c.value;
+    public static dateFormatValidator(dateCtrl: Control) {
+        let v = dateCtrl.value;
         return v !== null && Utils.parseDate(v) === null ? {
             validateDate: {
                 valid: false
             }
         } : null;
     }
-    public static validateABNFormat = (abn: Control) => {
-        if (/^(\d *?){11}$/.test(abn.value)) {
+    public static validateABNFormat = (abnCtrl: Control) => {
+        if (/^(\d *?){11}$/.test(abnCtrl.value)) {
             return null;
         } else {
             return { hasValidABN: { valid: false } };
         }
     }
 
-    public static validateUDNFormat = (abn: Control) => {
-        if (/^(\d *?)$/.test(abn.value)) {
+    public static validateUDNFormat = (udnCtrl: Control) => {
+        if (/^(\d *?)$/.test(udnCtrl.value)) {
             return null;
         } else {
             return { hasValidUDN: { valid: false } };
         }
     }
 
-    public static validateEmail = (abn: Control) => {
+    public static validateEmailFormat = (emailCtrl: Control) => {
         const EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-        if (EMAIL_REGEXP.test(abn.value)) {
+        if (EMAIL_REGEXP.test(emailCtrl.value)) {
             return null;
         } else {
             return { hasValidEmail: { valid: false } };
