ea2efe460bb840884d6e2bb0c7ec7977acfab748
Dave Cheong <dave@mobiusly.com>
2016-06-15 12:41:54 +1000

    Added findPendingByInvitationCodeInDateRange() with test

--- backend/typescript/models/relationship.model.ts
+++ backend/typescript/models/relationship.model.ts
@@ -3,6 +3,7 @@ import {RAMEnum, IRAMObject, RAMSchema, Query} from './base';
 import {IParty, PartyModel} from './party.model';
 import {IName, NameModel} from './name.model';
 import {IRelationshipType} from './relationshipType.model';
+import {IdentityModel} from './identity.model';
 import {
     HrefValue,
     Relationship as DTO,
@@ -116,6 +117,7 @@ export interface IRelationship extends IRAMObject {
 
 export interface IRelationshipModel extends mongoose.Model<IRelationship> {
     findByIdentifier:(id:String) => mongoose.Promise<IRelationship>;
+    findPendingByInvitationCodeInDateRange:(invitationCode:String, date:Date) => mongoose.Promise<IRelationship>;
     search:(subjectIdentityIdValue:string, delegateIdentityIdValue:string, page:number, pageSize:number) => Promise<SearchResult<IRelationship>>;
 }
 
@@ -154,6 +156,23 @@ RelationshipSchema.static('findByIdentifier', (id:String) => {
     return this.RelationshipModel
         .findOne({
             _id: id
+        })
+        .deepPopulate([
+            'relationshipType',
+            'subject',
+            'subjectNickName',
+            'delegate',
+            'delegateNickName'
+        ])
+        .exec();
+});
+
+RelationshipSchema.static('findPendingByInvitationCodeInDateRange', async (invitationCode:String, date:Date) => {
+    const identity = await IdentityModel.findPendingByInvitationCodeInDateRange(invitationCode, date);
+    const delegate = identity.party;
+    return this.RelationshipModel
+        .findOne({
+            delegate: delegate
         })
         .deepPopulate([
             'relationshipType',
--- backend/typescript/tests/relationship.model.spec.ts
+++ backend/typescript/tests/relationship.model.spec.ts
@@ -1,20 +1,26 @@
 import {connectDisconnectMongo} from './helpers';
+import {Seeder} from '../seeding/seed';
+import {
+    IIdentity,
+    IdentityModel,
+    IdentityType,
+    IdentityInvitationCodeStatus} from '../models/identity.model';
 import {
     IName,
-    NameModel
-} from '../models/name.model';
+    NameModel} from '../models/name.model';
+import {
+    IProfile,
+    ProfileModel,
+    ProfileProvider} from '../models/profile.model';
 import {
     IParty,
     PartyModel,
-    PartyType
-} from '../models/party.model';
-import {
+    PartyType} from '../models/party.model';import {
     IRelationship,
     RelationshipModel,
     RelationshipStatus
 } from '../models/relationship.model';
-import {IRelationshipType, RelationshipTypeModel} from '../models/relationshipType.model';
-import {Seeder} from '../seeding/seed';
+import {IRelationshipType} from '../models/relationshipType.model';
 
 /* tslint:disable:max-func-body-length */
 describe('RAM Relationship', () => {
@@ -27,7 +33,11 @@ describe('RAM Relationship', () => {
     let subjectParty1:IParty;
 
     let delegateNickName1:IName;
+    let delegateProfile1:IProfile;
     let delegateParty1:IParty;
+
+    let delegateIdentity1:IIdentity;
+    let relationship1:IRelationship;
 
     beforeEach((done) => {
 
@@ -37,10 +47,11 @@ describe('RAM Relationship', () => {
             .then()
             .then(Seeder.resetDataInMongo)
             .then(Seeder.loadReference)
-            .then(async ()=> {
+            .then(async () => {
+
                 try {
 
-                    relationshipTypeCustom = await RelationshipTypeModel.findByCodeIgnoringDateRange('CUSTOM_REPRESENTATIVE');
+                    relationshipTypeCustom = Seeder.custom_delegate_relationshipType;
 
                     subjectNickName1 = await NameModel.create({
                         givenName: 'Jane',
@@ -56,8 +67,32 @@ describe('RAM Relationship', () => {
                         familyName: 'Delegate 1'
                     });
 
+                    delegateProfile1 = await ProfileModel.create({
+                        provider: ProfileProvider.MyGov.name,
+                        name: delegateNickName1
+                    });
+
                     delegateParty1 = await PartyModel.create({
                         partyType: PartyType.Individual.name
+                    });
+
+                    delegateIdentity1 = await IdentityModel.create({
+                        identityType: IdentityType.InvitationCode.name,
+                        defaultInd: true,
+                        invitationCodeStatus: IdentityInvitationCodeStatus.Pending.name,
+                        invitationCodeExpiryTimestamp: new Date(2055, 1, 1),
+                        profile: delegateProfile1,
+                        party: delegateParty1
+                    });
+
+                    relationship1 = await RelationshipModel.create({
+                        relationshipType: relationshipTypeCustom,
+                        subject: subjectParty1,
+                        subjectNickName: subjectNickName1,
+                        delegate: delegateParty1,
+                        delegateNickName: delegateNickName1,
+                        startTimestamp: new Date(),
+                        status: RelationshipStatus.Pending.name
                     });
 
                 } catch (e) {
@@ -70,10 +105,64 @@ describe('RAM Relationship', () => {
             });
     });
 
-    it('inserts with no end timestamp', async(done) => {
+    it('finds by identifier', async (done) => {
         try {
 
-            const instance:IRelationship = await RelationshipModel.create({
+            const retrievedInstance = await RelationshipModel.findByIdentifier(relationship1.id);
+
+            expect(retrievedInstance).not.toBeNull();
+            expect(retrievedInstance.id).not.toBeNull();
+            expect(retrievedInstance.id).toBe(relationship1.id);
+
+            done();
+
+        } catch (e) {
+            fail('Because ' + e);
+            done();
+        }
+    });
+
+    it('finds by identifier', async (done) => {
+        try {
+
+            const retrievedInstance = await RelationshipModel.findByIdentifier(relationship1.id);
+
+            expect(retrievedInstance).not.toBeNull();
+            expect(retrievedInstance.id).not.toBeNull();
+            expect(retrievedInstance.id).toBe(relationship1.id);
+
+            done();
+
+        } catch (e) {
+            fail('Because ' + e);
+            done();
+        }
+    });
+
+    it('finds by invitation code in date range', async (done) => {
+        try {
+
+            const retrievedInstance = await RelationshipModel.findPendingByInvitationCodeInDateRange(
+                delegateIdentity1.rawIdValue,
+                new Date()
+            );
+
+            expect(retrievedInstance).not.toBeNull();
+            expect(retrievedInstance.id).not.toBeNull();
+            expect(retrievedInstance.id).toBe(relationship1.id);
+
+            done();
+
+        } catch (e) {
+            fail('Because ' + e);
+            done();
+        }
+    });
+
+    it('inserts with no end timestamp', async (done) => {
+        try {
+
+            const instance = await RelationshipModel.create({
                 relationshipType: relationshipTypeCustom,
                 subject: subjectParty1,
                 subjectNickName: subjectNickName1,
@@ -98,10 +187,10 @@ describe('RAM Relationship', () => {
         }
     });
 
-    it('inserts with end timestamp', async(done) => {
+    it('inserts with end timestamp', async (done) => {
         try {
 
-            const instance:IRelationship = await RelationshipModel.create({
+            const instance = await RelationshipModel.create({
                 relationshipType: relationshipTypeCustom,
                 subject: subjectParty1,
                 subjectNickName: subjectNickName1,
