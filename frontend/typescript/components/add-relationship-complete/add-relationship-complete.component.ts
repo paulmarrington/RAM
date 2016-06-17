f619822e87fe0de8dbc5dd516bfb7b3dcdcb2619
Ali Salehi <ali.salehi@ato.gov.au>
2016-06-17 15:33:06 +1000

    linked back to relationship added binding for variables

--- frontend/typescript/components/add-relationship-complete/add-relationship-complete.component.ts
+++ frontend/typescript/components/add-relationship-complete/add-relationship-complete.component.ts
@@ -1,19 +1,17 @@
 import {RAMNgValidators} from '../../commons/ram-ng-validators';
 import {OnInit, Component} from '@angular/core';
 import {Validators, ControlGroup, FormBuilder, FORM_DIRECTIVES} from '@angular/common';
-import {ROUTER_PROVIDERS, RouteParams} from '@angular/router-deprecated';
+import {ROUTER_DIRECTIVES, RouteParams} from '@angular/router-deprecated';
 
 @Component({
     selector: 'add-relationship-complete',
     templateUrl: 'add-relationship-complete.component.html',
-    directives: [FORM_DIRECTIVES],
-    providers: [ROUTER_PROVIDERS]
+    directives: [ROUTER_DIRECTIVES,FORM_DIRECTIVES],
+    providers: []
 })
 export class AddRelationshipCompleteComponent implements OnInit {
 
     public form: ControlGroup;
-
-    public data: { email?: String, udn?: String } = { email: 'test1', udn: 'test2' };
 
     public code: string;
 
@@ -23,14 +21,12 @@ export class AddRelationshipCompleteComponent implements OnInit {
     }
 
     public ngOnInit() {
-                this.code = this.routeParams.get('invitationCode');
+        this.code = this.routeParams.get('invitationCode');
         this.idValue = this.routeParams.get('idValue');
 
         this.form = this._fb.group({
-            'email': [this.data.email,
-                Validators.compose([RAMNgValidators.validateEmailFormat])],
-            'udn': [this.data.udn,
-                Validators.compose([RAMNgValidators.validateUDNFormat])]
+            'email': ['', Validators.compose([RAMNgValidators.validateEmailFormat])],
+            'udn': ['', Validators.compose([RAMNgValidators.validateUDNFormat])]
         });
 
     }
