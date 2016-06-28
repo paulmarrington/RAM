f93279dd4f656ff51204b475d3699df36777ba69
Dave Cheong <dave@mobiusly.com>
2016-06-28 12:04:54 +1000

    Redirect home to the relationships page if logged in, otherwise display some placeholder landing page message

--- frontend/typescript/components/ram/ram.component.ts
+++ frontend/typescript/components/ram/ram.component.ts
@@ -1,9 +1,23 @@
 // TODO this component need to be deleted or migrated to project standards
 import {Component} from '@angular/core';
+import {Router} from '@angular/router-deprecated';
+import {RAMRestService} from '../../services/ram-rest.service';
 
 @Component({
     selector: 'ram-app',
     templateUrl: 'ram.component.html'
 })
 export class RamComponent {
+
+    constructor(private router: Router,
+                private rest: RAMRestService) {
+    }
+
+    public ngOnInit() {
+        this.rest.findMyIdentity().subscribe(identity => {
+            const idValue = identity.idValue;
+            this.router.navigate(['Relationships', { idValue: idValue }]);
+        });
+    }
+
 }
