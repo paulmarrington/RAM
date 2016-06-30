80004b1335bc23a9fca282a2f00717c5727fbbef
Dave Cheong <dave@mobiusly.com>
2016-06-30 10:54:38 +1000

    Displaying subject headings

--- frontend/typescript/components/relationships/relationships.component.ts
+++ frontend/typescript/components/relationships/relationships.component.ts
@@ -6,8 +6,11 @@ import Rx from 'rxjs/Rx';
 import {RAMRestService} from '../../services/ram-rest.service';
 import {RAMIdentityService} from '../../services/ram-identity.service';
 import {
+    ISearchResult,
     IName,
-    IRelationshipType, IHrefValue
+    IParty,
+    IRelationshipType,
+    IHrefValue
 } from '../../../../commons/RamAPI2';
 
 @Component({
@@ -21,8 +24,11 @@ export class RelationshipsComponent {
     public idValue:string;
 
     public identityDisplayName$:Rx.Observable<IName>;
+    public subjectsResponse$:Rx.Observable<ISearchResult<IHrefValue<IParty>[]>>;
 
     public relationshipTypes: IHrefValue<IRelationshipType>[] = [];
+
+    private _isLoading = false; // set to true when you want the UI indicate something is getting loaded.
 
     constructor(private routeParams:RouteParams,
                 private identityService:RAMIdentityService,
@@ -33,6 +39,12 @@ export class RelationshipsComponent {
         this.idValue = this.routeParams.get('idValue');
         this.identityDisplayName$ = this.identityService
             .getDefaultName(this.idValue).map(this.displayName);
+        this.subjectsResponse$ = this.rest.searchDistinctSubjectsBySubjectOrDelegateIdentity(this.idValue, 1);
+        this.subjectsResponse$.subscribe((searchResult) => {
+            console.log('SEARCH RESULT!!!!');
+            console.log('totalCount = ' + searchResult.totalCount);
+            console.log('pageSize = ' + searchResult.pageSize);
+        });
         this.rest.listRelationshipTypes().subscribe((relationshipTypes) => {
             this.relationshipTypes = relationshipTypes;
         });
@@ -42,6 +54,25 @@ export class RelationshipsComponent {
         if (name) {
             return name.unstructuredName ? name.unstructuredName : name.givenName + ' ' + name.familyName;
         }
+        return '';
+    }
+
+    public displayNameForSubject(subject:IParty):string {
+        if (subject && subject.identities && subject.identities.length > 0) {
+            for (const identityHrefValue of subject.identities) {
+                const identity = identityHrefValue.value;
+                if (identity.defaultInd) {
+                    const name = identity.profile.name;
+                    return this.displayName(name);
+                }
+            }
+            return this.displayName(subject.identities[0].value.profile.name);
+        }
+        return '';
+    }
+
+    public get isLoading() {
+        return this._isLoading;
     }
 
 }
