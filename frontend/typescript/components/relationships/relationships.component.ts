178f7d4f82ffec7959b33ae5fa8d0934f8ce6f59
Dave Cheong <dave@mobiusly.com>
2016-07-07 12:27:18 +1000

    New relationships listing, work in progress

--- frontend/typescript/components/relationships/relationships.component.ts
+++ frontend/typescript/components/relationships/relationships.component.ts
@@ -118,52 +118,6 @@ export class RelationshipsComponent implements OnInit, OnDestroy {
         return providerNames.join(',');
     }
 
-    // todo deprecated
-    public getOtherPartyHrefValue = (relationship: IRelationship) => {
-        if (this.subjectHrefValue) {
-            const relationshipHref = this.modelHelper.linkByType('self', relationship.subject._links).href;
-            const subjectHref = this.modelHelper.linkByType('self', this.subjectHrefValue._links).href;
-            if (relationshipHref === subjectHref) {
-                return relationship.delegate;
-            } else {
-                return relationship.subject;
-            }
-        }
-        return null;
-    };
-
-    public getOtherParty = (relationship: IRelationship) => {
-        const party = this.getOtherPartyHrefValue(relationship);
-        return party ? party.value : null;
-    };
-
-    public backToListing = () => {
-        this.subjectHrefValue = null;
-    };
-
-    // todo deprecated
-    public expandSubject = (subjectHrefValue: IHrefValue<IParty>) => {
-        this._isLoading = true;
-        this.subjectHrefValue = subjectHrefValue;
-        if (subjectHrefValue.value.identities.length > 0) {
-            const idValue = subjectHrefValue.value.identities[0].value.idValue;
-            this.relationshipsResponse$ = this.rest.searchRelationshipsByIdentity(idValue, 1);
-            this.relationshipsResponse$.subscribe(() => {
-                this._isLoading = false;
-            }, (err) => {
-                alert(JSON.stringify(err, null, 4));
-                this._isLoading = false;
-            });
-        }
-    };
-
-    // todo deprecated
-    // todo not sure what the drill down conditional logic is, for now assume UNIVERSAL
-    public isDrillDownPossible = (relationship: IRelationship) => {
-        let relationshipType = this.modelHelper.getRelationshipType(this.relationshipTypes, relationship);
-        return relationshipType && relationshipType.code === 'UNIVERSAL_REPRESENTATIVE';
-    };
-
     public get isLoading() {
         return this._isLoading;
     }
