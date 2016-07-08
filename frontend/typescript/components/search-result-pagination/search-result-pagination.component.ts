2abc3fcbb515e958f3b83de668d4a0e20415ff56
Dave Cheong <dave@mobiusly.com>
2016-07-08 11:00:12 +1000

    SearchResult pagination, work in progress

--- frontend/typescript/components/search-result-pagination/search-result-pagination.component.ts
+++ frontend/typescript/components/search-result-pagination/search-result-pagination.component.ts
@@ -11,4 +11,44 @@ export class SearchResultPaginationComponent {
 
     @Input() public searchResult: ISearchResult<Object>;
 
+    public pages(): number[] {
+        return [1,2,3,4,5];
+    }
+
+    public isPreviousDisabled(): boolean {
+        return true;
+    }
+
+    public isNextDisabled(): boolean {
+        return false;
+    }
+
+    public hasPreviousEllipsis(): boolean {
+        return true;
+    }
+
+    public hasNextEllipsis(): boolean {
+        return true;
+    }
+
+    public goToPage(page: number) {
+        if (page != this.searchResult.page) {
+            alert('TODO: Go To Page: ' + page);
+        }
+    }
+
+    public goToPreviousPage() {
+        if (!this.isPreviousDisabled()) {
+            const previousPage = this.searchResult.page - 1;
+            alert('TODO: Go To Previous Page: ' + previousPage);
+        }
+    }
+
+    public goToNextPage() {
+        if (!this.isNextDisabled()) {
+            const nextPage = this.searchResult.page + 1;
+            alert('TODO: Go To Next Page: ' + nextPage);
+        }
+    }
+
 }
