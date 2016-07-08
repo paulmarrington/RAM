3a7a4f4c1b26086f2a9a0d4cf950a57d1ec5e29e
Dave Cheong <dave@mobiusly.com>
2016-07-08 22:38:08 +1000

    Added router params helper

---
+++ frontend/typescript/commons/router-params-helper.ts
@@ -0,0 +1,20 @@
+import Rx from 'rxjs/Rx';
+import {ActivatedRoute, Router, Params} from '@angular/router';
+
+export class RouterParamsHelper {
+
+    public static params(route: ActivatedRoute, router: Router) {
+
+        const pathParams$ = route.params;
+        const queryParams$ = router.routerState.queryParams;
+
+        return Rx.Observable.zip(pathParams$, queryParams$, (pathParams: Params, queryParams: Params) => {
+            return {
+                path: pathParams,
+                query: queryParams
+            };
+        });
+
+    }
+
+}
