af9b92e10fc60f0a4b2e77aa615d09f3ed71f863
Dave Cheong <dave@mobiusly.com>
2016-07-09 22:10:09 +1000

    Move my-identity into commons

--- frontend/typescript/components/app/app.component.ts
+++ frontend/typescript/components/app/app.component.ts
@@ -2,11 +2,13 @@ import 'ng2-bootstrap';
 import {Component} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_DIRECTIVES } from '@angular/router';
+
 import {RAMModelHelper} from '../../commons/ram-model-helper';
 import {RAMRestService} from '../../services/ram-rest.service';
 import {RAMNavService} from '../../services/ram-nav.service';
 import {RAMConstantsService} from '../../services/ram-constants.service';
-import {MyIdentityComponent} from '../my-identity/my-identity.component';
+
+import {MyIdentityComponent} from '../commons/my-identity/my-identity.component';
 import {ErrorComponent} from '../error/error.component';
 import {ErrorService} from '../error/error.service';
 
